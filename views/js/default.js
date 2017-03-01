8378cd6dbf06e27424e2f1ad9e3d50348cae54a9
AnirudhGP <anirudh.gp@gmail.com>
2017-03-01 13:38:48 +0530

    Almost final

--- views/js/default.js
+++ views/js/default.js
@@ -30,24 +30,3 @@ $('.carousel').swipeleft(function() {
   $(this).carousel('next');
 });
 
-// Google Map
-function googleMap() {
-  var map = document.getElementById('google-map');
-
-  var map_options = {
-    center: new google.maps.LatLng(-33.867363,151.183539),
-    scrollwheel: false,
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(map, map_options)
-
-  // Map Marker
-  var myLatlng = new google.maps.LatLng(-33.867363,151.183539);
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    icon: 'img/location-pin.svg'
-  });
-}
-google.maps.event.addDomListener(window, 'load', googleMap);
